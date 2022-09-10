0941fe5d4cd06156398d84738e0b9ee6a898132a
R-Mohammed-Hasan <mohammed.hasan@fwsa.freshworks.com>
2022-09-10 20:31:05 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import './App.css';
-import React from 'react';
+import "./App.css";
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
